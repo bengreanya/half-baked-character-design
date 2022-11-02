4bc2d3a86e743034b28efcadd14973526fb9827d
Ben Greanya <bengreanya@gmail.com>
2022-11-02 11:44:56 -0700

    added middle functions

--- app.js
+++ app.js
@@ -17,21 +17,17 @@ let bottomCount = 0;
 // set state for all of the character's catchphrases
 const catchphrases = [];
 headDropdown.addEventListener("change", () => {
-  // get the value of the head dropdown
   const value = headDropdown.value;
-  // increment the head change count stateP
   headCount++;
-  // update the dom for the head (use style.backgroundImage on the headEl div instead of trying to set the .src -- it's NOT an img tag!)
   headEl.style.backgroundImage = `url("./assets/${value}-head.png")`;
-  // update the stats to show the new count (call displayStats() to do this work)
   displayStats();
 });
 
 middleDropdown.addEventListener("change", () => {
-  // get the value of the middle dropdown
-  // increment the middle change count state
-  // update the dom for the middle (NOTE: use style.backgroundImage on the middleEl div instead of trying to set the .src -- it's NOT an img tag!)
-  // update the stats to show the new count (call displayStats() to do this work)
+  const value = middleDropdown.value;
+  middleCount++;
+  middleEl.style.backgroundImage = `url("./assets/${value}-middle.png")`;
+  displayStats();
 });
 
 bottomDropdown.addEventListener("change", () => {
